46a9ca86bc909e74ce0c322fa6809e904d83efb4
gabriel.melchor@softtek.com <gabriel.melchor@softtek.com>
2019-08-28 10:49:47 -0700

    Added Item ctrler

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        public ItemController(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+
+        public ApplicationDbContext _context { get; }
+
+        public IActionResult Index()
+        {
+            var items = this._context.Items.ToList();
+            return View("Index",items);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            this._context.Add(item);
+            this._context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int Id)
+        {
+            var item = this._context.Find(Id);
+            this._context.Remove(item);
+            this._context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
